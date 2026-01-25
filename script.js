638ff5515734b10cf20ce5d248b65efc92b8a526
MyjopFreelance <my.jop.freelance@gmail.com>
2026-01-25 17:46:54 +0200

    Refactor setMood function for improved text formatting
    Updated text content for mood messages to include spaces.

--- script.js
+++ script.js
@@ -5,18 +5,19 @@ function setMood(mood) {
   if (mood === "happy") {
     document.body.style.background = "#fff6c7";
     emoji.textContent = "😊";
-    text.textContent = "Today is a wonderful day!";
+    text.textContent = " Today is a wonderful day !";
   }
 
   if (mood === "calm") {
     document.body.style.background = "#dff5f0";
     emoji.textContent = "😐";
-    text.textContent = "Calm and balance";
+    text.textContent = " Calm and balance ";
   }
 
   if (mood === "sad") {
     document.body.style.background = "#e6e6e6";
     emoji.textContent = "😢";
-    text.textContent = "Everything will be fine";
+    text.textContent = " Everything will be fine ";
   }
 }
+
